1a40b2d31b3a5e6526945b012b86c624f7d63779
Brendan Hansen <brendan.f.hansen@gmail.com>
2020-09-19 22:06:26 -0500

    bugfix with struct literals as struct member default values

--- src/onyxsymres.c
+++ src/onyxsymres.c
@@ -262,9 +262,9 @@ static void symres_unaryop(AstUnaryOp** unaryop) {
 
 static void symres_struct_literal(AstStructLiteral* sl) {
     if (sl->stnode != NULL) symres_expression(&sl->stnode);
-    if (sl->stnode == NULL || sl->stnode->kind == Ast_Kind_Error) return;
+    if (sl->stnode == NULL || sl->stnode->kind == Ast_Kind_Error || sl->stnode->kind == Ast_Kind_Symbol) return;
 
-    sl->type_node = (AstType *) sl->stnode;
+    sl->type_node = symres_type((AstType *) sl->stnode);
     sl->type = type_build_from_ast(semstate.allocator, sl->type_node);
 
     if (!type_is_structlike_strict(sl->type)) {
