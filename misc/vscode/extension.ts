cd6333c1049a76459a9e3ebd2fdbeb86e164b219
Brendan Hansen <brendan.f.hansen@gmail.com>
2021-11-24 14:58:53 -0600

    too much work on getting vscode to know symbols

---
+++ misc/vscode/extension.ts
@@ -0,0 +1,21 @@
+import * as vscode from "vscode";
+import * as vsctmls from 'vscode-textmate-languageservice';
+
+export async function activate(context: vscode.ExtensionContext) {
+	let console = vscode.window.createOutputChannel("Onyx Extension");
+	console.appendLine("Starting Onyx Extension");
+
+	const selector: vscode.DocumentSelector = { language: 'onyx', scheme: "file" };
+	const engine = new vsctmls.textmateEngine.TextmateEngine('onyx', 'source.onyx');
+	const documentSymbolProvider = new vsctmls.documentSymbols.DocumentSymbolProvider(engine);
+	const workspaceSymbolProvider = new vsctmls.workspaceSymbols.WorkspaceSymbolProvider('onyx', documentSymbolProvider);
+	// const foldingProvider = new vsctmls.folding.FoldingProvider(engine);
+	// const peekFileDefinitionProvider = new vsctmls.peekDefinitions.PeekDefinitionProvider(documentSymbolProvider);
+
+	context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(selector, documentSymbolProvider));
+	context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(workspaceSymbolProvider));
+	// context.subscriptions.push(vscode.languages.registerFoldingRangeProvider(selector, foldingProvider));
+	// context.subscriptions.push(vscode.languages.registerDefinitionProvider(['onyx'], peekFileDefinitionProvider));
+
+	console.appendLine("Onyx Extension loaded.");
+}
---
+++ misc/vscode/out/extension.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+const vscode = require("vscode");
+const vsctmls = require("vscode-textmate-languageservice");
+function activate(context) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let console = vscode.window.createOutputChannel("Onyx Extension");
+        console.appendLine("Starting Onyx Extension");
+        const selector = { language: 'onyx', scheme: "file" };
+        const engine = new vsctmls.textmateEngine.TextmateEngine('onyx', 'source.onyx');
+        const documentSymbolProvider = new vsctmls.documentSymbols.DocumentSymbolProvider(engine);
+        const workspaceSymbolProvider = new vsctmls.workspaceSymbols.WorkspaceSymbolProvider('onyx', documentSymbolProvider);
+        // const foldingProvider = new vsctmls.folding.FoldingProvider(engine);
+        // const peekFileDefinitionProvider = new vsctmls.peekDefinitions.PeekDefinitionProvider(documentSymbolProvider);
+        context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(selector, documentSymbolProvider));
+        context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(workspaceSymbolProvider));
+        // context.subscriptions.push(vscode.languages.registerFoldingRangeProvider(selector, foldingProvider));
+        // context.subscriptions.push(vscode.languages.registerDefinitionProvider(['onyx'], peekFileDefinitionProvider));
+        console.appendLine("Onyx Extension loaded.");
+    });
+}
+exports.activate = activate;
