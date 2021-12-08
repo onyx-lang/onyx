b4fe411aa506cc88085738c5126f5ddee3556370
Brendan Hansen <brendan.f.hansen@gmail.com>
2021-12-07 22:08:48 -0600

    finalized fixing linux build scripts

--- modules/glfw3/build.sh
+++ modules/glfw3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # To be run from the project root
 
-gcc -O2 -shared -fPIC ./modules/glfw3/onyx_glfw3.c -I include -I lib/linux_x86_64/include -lglfw -o modules/glfw3/onyx_glfw3.so
+gcc -O2 -shared -fPIC ./modules/glfw3/onyx_glfw3.c -I include -I lib/common/include -lglfw -o modules/glfw3/onyx_glfw3.so
--- modules/opengles/build.sh
+++ modules/opengles/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -shared -fPIC modules/opengles/onyx_opengles.c -I include -I lib/linux_x86_64/include -o ./modules/opengles/onyx_opengles.so -lGL
+gcc -shared -fPIC modules/opengles/onyx_opengles.c -I include -I lib/common/include -o ./modules/opengles/onyx_opengles.so -lGL
