c78581c5b20679dec6d887fe48d5768fc7b0168f
Brendan Hansen <brendan.f.hansen@gmail.com>
2022-08-30 21:52:57 -0500

    improvements to cbindgen and cptr

--- src/onyx_runtime.c
+++ src/onyx_runtime.c
@@ -1341,6 +1341,16 @@ ONYX_DEF(__cptr_read_u64, (WASM_I64), (WASM_I64)) {
     return NULL;
 }
 
+ONYX_DEF(__cptr_extract_str, (WASM_I64, WASM_I32, WASM_I32), (WASM_I32)) {
+    unsigned int len = strlen((char *) params->data[0].of.i64);
+
+    if (params->data[2].of.i32 != 0) {
+        strncpy(ONYX_PTR(params->data[1].of.i32), (char *) params->data[0].of.i64, params->data[2].of.i32);
+    }
+
+    results->data[0] = WASM_I32_VAL(len);
+    return NULL;
+}
 
 ONYX_LIBRARY {
     ONYX_FUNC(__file_open_impl)
@@ -1409,6 +1419,7 @@ ONYX_LIBRARY {
     ONYX_FUNC(__cptr_read_u16)
     ONYX_FUNC(__cptr_read_u32)
     ONYX_FUNC(__cptr_read_u64)
+    ONYX_FUNC(__cptr_extract_str)
 
     NULL
 };
