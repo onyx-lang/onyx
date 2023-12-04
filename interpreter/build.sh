fffc188475d0c2bcba9eb91b7da631d4bc215603
Brendan Hansen <brendan.f.hansen@gmail.com>
2023-12-04 11:11:22 -0600

    added: MacOS builds for OVM in GitHub Actions

--- interpreter/build.sh
+++ interpreter/build.sh
@@ -8,6 +8,10 @@ TARGET="../shared/lib/$ONYX_ARCH/lib/libovmwasm.a"
 C_FILES="src/ovmwasm.c src/vm/*.c src/wasm/*.c src/debug/*.c"
 INCLUDES="-I../shared/include -Iinclude"
 
+if [ ! -z ${ONYX_TARGET+x} ]; then
+    FLAGS="$FLAGS --target=$ONYX_TARGET"
+fi
+
 mkdir -p "build_tmp"
 
 echo "Compiling ovmwasm to $TARGET"
