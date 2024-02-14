275a6936d1e85b73f3811c8d7df7f136e4f51cf1
Brendan Hansen <brendan.f.hansen@gmail.com>
2024-02-14 15:43:31 -0600

    added: better VS Code support for debug session selection

--- misc/vscode/out/extension.js
+++ misc/vscode/out/extension.js
@@ -12,12 +12,64 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const vsctmls = require("vscode-textmate-languageservice");
+const fs = require("fs");
 const vslc = require("vscode-languageclient/node");
 let client;
+function get_onyx_path() {
+    let onyx_path = process.env['ONYX_PATH'];
+    if (!onyx_path) {
+        onyx_path = require('os').homedir() + "/.onyx";
+        process.env["ONYX_PATH"] = onyx_path;
+        process.env["PATH"] = process.env["PATH"] + ":" + onyx_path + "/bin";
+    }
+    return onyx_path;
+}
+function registerCommands() {
+    vscode.commands.registerCommand('extension.pickOnyxSession', () => {
+        return new Promise((res, rej) => {
+            let onyx_path = get_onyx_path();
+            const sessionEntries = fs
+                .readdirSync(onyx_path)
+                .filter(file => file.startsWith("debug."))
+                .map(file => ({
+                label: file.split(".")[1],
+                pid: parseInt(file.split(".")[1])
+            }));
+            if (sessionEntries.length == 0) {
+                rej(new Error("No sessions waiting to be debugged"));
+                return;
+            }
+            if (sessionEntries.length == 1) {
+                res(`${onyx_path}/debug.${sessionEntries[0].pid}`);
+                return;
+            }
+            const quickPick = vscode.window.createQuickPick();
+            quickPick.title = "Attach to session";
+            quickPick.canSelectMany = false;
+            quickPick.matchOnDescription = true;
+            quickPick.matchOnDetail = true;
+            quickPick.placeholder = "Select the session to attach to";
+            quickPick.items = sessionEntries;
+            quickPick.onDidAccept(() => {
+                if (quickPick.selectedItems.length !== 1) {
+                    rej(new Error(`Session not selected. case one`));
+                }
+                const selectedId = quickPick.selectedItems[0].pid;
+                quickPick.dispose();
+                res(`${onyx_path}/debug.${selectedId}`);
+            });
+            quickPick.onDidHide(() => {
+                quickPick.dispose();
+            });
+            quickPick.show();
+        });
+    });
+}
 function activate(context) {
     return __awaiter(this, void 0, void 0, function* () {
         let console = vscode.window.createOutputChannel("Onyx Extension");
         console.appendLine("Starting Onyx Extension");
+        registerCommands();
         const selector = { language: 'onyx', scheme: "file" };
         const engine = new vsctmls.textmateEngine.TextmateEngine('onyx', 'source.onyx');
         const documentSymbolProvider = new vsctmls.documentSymbols.DocumentSymbolProvider(engine);
@@ -26,12 +78,7 @@ function activate(context) {
         context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(selector, documentSymbolProvider));
         context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(workspaceSymbolProvider));
         context.subscriptions.push(vscode.languages.registerDefinitionProvider({ 'language': 'onyx' }, peekFileDefinitionProvider));
-        let onyx_path = process.env['ONYX_PATH'];
-        if (!onyx_path) {
-            onyx_path = require('os').homedir() + "/.onyx";
-            process.env["ONYX_PATH"] = onyx_path;
-            process.env["PATH"] = process.env["PATH"] + ":" + onyx_path + "/bin";
-        }
+        let onyx_path = get_onyx_path();
         let executable = `${onyx_path}/bin/onyx`;
         if (process.platform == "win32") {
             // Windows distributions are different
